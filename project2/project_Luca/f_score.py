2d70955a12b393c8fe3a4512d03887fc00c43b47
zampieri <zluca.lucky@gmail.com>
2017-12-06 23:51:41 +0100

    not functioning version of fscore

---
+++ project2/project_Luca/f_score.py
@@ -0,0 +1,28 @@
+"""File with funtion for computing the F-Score """
+
+# Extract label images
+def extract_labels_for_validation(foldername, num_images, train=True):
+    """Extract the labels into a 1-hot matrix [image index, label index]."""
+    gt_imgs = []
+    for i in range(1, num_images+1):
+        imageid = "predicted_groundtruth_" + str(i)
+        image_filename = filename + imageid + ".png"
+        if os.path.isfile(image_filename):
+            print ('Loading ' + image_filename)
+            img = mpimg.imread(image_filename)
+            gt_imgs.append(img)
+        else:
+            print ('File ' + image_filename + ' does not exist')
+
+    num_images = len(gt_imgs)
+    gt_patches = [img_crop(gt_imgs[i], IMG_PATCH_SIZE, IMG_PATCH_SIZE) for i in range(num_images)]
+    data = numpy.asarray([gt_patches[i][j] for i in range(len(gt_patches)) for j in range(len(gt_patches[i]))])
+    labels = numpy.asarray([value_to_class(numpy.mean(data[i])) for i in range(len(data))])
+
+    # Convert to dense 1-hot representation.
+    return labels.astype(numpy.float32)
+
+
+def compute_FScore(gt_folder,prediction_folder, num_image):
+    labels = extract_labels(gt_folder, num_images)
+    predictions = extract_labels_for_validation(prediction_folder, num_image)
