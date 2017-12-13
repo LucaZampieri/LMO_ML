b0d19a681d92dce80541ea8f6bf86a5b1ddabe4c
zampieri <zluca.lucky@gmail.com>
2017-12-13 19:16:32 +0100

    not working

--- project2/project_Luca/unet/layers.py
+++ project2/project_Luca/unet/layers.py
@@ -2,12 +2,12 @@
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # tf_unet is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with tf_unet.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -39,10 +39,10 @@ def conv2d(x, W, keep_prob_):
 def deconv2d(x, W, stride):
     x_shape = tf.shape(x)
     output_shape = tf.stack([x_shape[0], x_shape[1]*2, x_shape[2]*2, x_shape[3]//2])
-    return tf.nn.conv2d_transpose(x, W, output_shape, strides=[1, stride, stride, 1], padding='VALID')
+    return tf.nn.conv2d_transpose(x, W, output_shape, strides=[1, stride, stride, 1], padding='SAME')
 
 def max_pool(x,n):
-    return tf.nn.max_pool(x, ksize=[1, n, n, 1], strides=[1, n, n, 1], padding='VALID')
+    return tf.nn.max_pool(x, ksize=[1, n, n, 1], strides=[1, n, n, 1], padding='SAME')
 
 def crop_and_concat(x1,x2):
     x1_shape = tf.shape(x1)
@@ -51,7 +51,7 @@ def crop_and_concat(x1,x2):
     offsets = [0, (x1_shape[1] - x2_shape[1]) // 2, (x1_shape[2] - x2_shape[2]) // 2, 0]
     size = [-1, x2_shape[1], x2_shape[2], -1]
     x1_crop = tf.slice(x1, offsets, size)
-    return tf.concat([x1_crop, x2], 3)   
+    return tf.concat([x1_crop, x2], 3)
 
 def pixel_wise_softmax(output_map):
     exponential_map = tf.exp(output_map)
